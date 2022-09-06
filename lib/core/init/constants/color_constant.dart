de97568ce232855a5bedf282b076222ceb4fbd13
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2022-09-06 11:35:39 +0300

    Color constants file added.

---
+++ lib/core/init/constants/color_constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFffffff);
+  static const Color white70 = Color(0xB3FFFFFF);
+  static const Color amaranth = Color(0xFFE91E63);
+  static const Color pomegranate = Color(0xFFE91E63);
+  static const Color dodgerBlue = Color(0xFF2196F3);
+  static const Color fruitSalad = Color(0xFF4CAF50);
+}
